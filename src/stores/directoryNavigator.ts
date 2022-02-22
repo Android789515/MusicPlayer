c6c2c4ed0b2af8072d1d408c2167961a7dc3ea9c
Android789515 <derflug@mailfence.com>
2022-02-22 12:26:17 -0600

    Refactored directoryNavigator and deleted unused touchAndHold.

--- src/stores/directoryNavigator.ts
+++ src/stores/directoryNavigator.ts
@@ -3,9 +3,7 @@ import { get, writable } from 'svelte/store'
 import QueuedSong from '../components/media-components/wrappers/QueuedSong.svelte'
 import Library from '../components/library-components/Library.svelte'
 
-type Directory = string
-
-const currentDirectory = writable<Directory>('')
+const currentDirectory = writable<string>('')
 
 const directories = {
     currentlyPlaying: QueuedSong,
