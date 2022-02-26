8c1cfb7df87cf5ebd38df541fdaaa1dff6027070
Android789515 <derflug@mailfence.com>
2022-02-25 18:31:43 -0600

    Reverted Songs component, created separate component SearchResults instead.

--- src/components/library-components/SearchResults.svelte
+++ src/components/library-components/SearchResults.svelte
@@ -1,78 +1,11 @@
 <script lang='ts'>
-    import { library, songs, queuedSong } from '../../stores/library'
-    import { useDirectoryNavigator } from '../../stores/directoryNavigator'
 
-    import SongInfo from '../wrappers/SongInfo.svelte'
-
-    const navigator = useDirectoryNavigator()
-
-    const handleClick = (callback) => {
-        callback()
-        navigator.navigate('currentlyPlaying')
-    }
-
-    const songStyles = {
-        textAlign: 'left',
-
-        display: 'grid',
-        gridTemplateRows: 'auto auto',
-        gridTemplateColumns: 'auto max-content',
-        gridRowGap: '.5em',
-        gridColumnGap: '1em',
-        gridAutoFlow: 'column',
-        alignItems: 'baseline',
-
-        paddingBottom: '1em',
-        borderBottom: 'solid .25em'
-    }
-
-    $: isSongQueued = $queuedSong.src !== undefined
 </script>
 
-<ul
-    aria-label='Songs'
-    class='songs unstyled-ul'
-    class:whenSongQueued={isSongQueued}
-    role='list'
->
-    {#each $songs as song (song.id)}
-        <li
-            class='song clickable'
-            on:click={() => handleClick(() => library.queueSong(song.id))}
-        >
-            <SongInfo
-                styles={songStyles}
-                songInfo={song}
-                timeToDisplay={song.duration}
-            />
-        </li>
-    {/each}
+<ul class='search-results'>
+
 </ul>
 
 <style>
-    .songs {
-        grid-column: 2 / span 2;
 
-        display: grid;
-        grid-row-gap: 1.25em;
-
-        overflow: auto;
-
-        max-height: 48em;
-
-        padding: 1em 0;
-    }
-
-    .songs.whenSongQueued {
-    }
-
-    .song {
-        padding-right: 1em;
-    }
-
-    .song-title {
-        font-size: 1.5rem;
-
-        margin: 0;
-    }
 </style>
