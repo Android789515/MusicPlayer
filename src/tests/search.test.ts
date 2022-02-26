bcacd7b2ed922991fb3026a34df0b205419a0150
Android789515 <derflug@mailfence.com>
2022-02-26 11:52:40 -0600

    Created test for search method to find every instance of a number in an array.

--- src/tests/search.test.ts
+++ src/tests/search.test.ts
@@ -1,7 +1,7 @@
 import { search } from '../utils/arrayMethods'
 
 describe('search method', () => {
-    test('finds every odd number in an array of numbers', () => {
-        expect()
+    test('finds every number 1 in an array of [1, 2, 3, 1, 5]', () => {
+        expect(search([1, 2, 3, 1, 5], 1)).toContainEqual([1, 1])
     })
 })
--- src/utils/arrayMethods.ts
+++ src/utils/arrayMethods.ts
@@ -3,10 +3,6 @@
 export const search = (array: any[], find: any) => {
     return array.reduce((results, element: any) => {
 
-        if (Array.isArray(element)) {
-            results = [...results, search(element, find)]
-        }
-
         return []
     }, [])
 }
