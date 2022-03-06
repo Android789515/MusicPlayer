386342b52ce845ea1d5b8df555154c1f3eed090f
Android789515 <derflug@mailfence.com>
2022-03-06 11:43:06 -0600

    Added more styles to playlist entries.

--- src/components/library-components/PlaylistEntry.svelte
+++ src/components/library-components/PlaylistEntry.svelte
@@ -6,18 +6,25 @@
     const numberOfSongs = songs.length + ' songs'
 </script>
 
-<li class='layout'>
-    <h2 class='title'>{name}</h2>
-    <p class='number-of-songs'>{numberOfSongs}</p>
+<li class='container'>
+    <div class='layout'>
+        <h2 class='title'>{name}</h2>
+        <p class='number-of-songs'>{numberOfSongs}</p>
+    </div>
 </li>
 
 <style>
+    .container {
+        padding-right: var(--inner-component-side-space);
+    }
+
     .layout {
         display: flex;
         justify-content: space-between;
         align-items: baseline;
 
-        padding-right: var(--inner-component-side-space);
+        padding-bottom: 1em;
+        border-bottom: solid .25em;
     }
 
     .layout > * {
