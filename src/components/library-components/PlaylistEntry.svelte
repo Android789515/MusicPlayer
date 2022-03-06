9bbc71040b5bd770ee37b4b4bb0418bc3b38fd68
Android789515 <derflug@mailfence.com>
2022-03-05 20:37:02 -0600

    Added basic styles to playlist entries.

--- src/components/library-components/PlaylistEntry.svelte
+++ src/components/library-components/PlaylistEntry.svelte
@@ -1,13 +1,34 @@
 <script lang='ts'>
     export let playlistInfo
 
-    const { name } = playlistInfo
+    const { name, songs } = playlistInfo
+
+    const numberOfSongs = songs.length + ' songs'
 </script>
 
-<li>
-    {name}
+<li class='layout'>
+    <h2 class='title'>{name}</h2>
+    <p class='number-of-songs'>{numberOfSongs}</p>
 </li>
 
 <style>
+    .layout {
+        display: flex;
+        justify-content: space-between;
+        align-items: baseline;
 
+        padding-right: var(--inner-component-side-space);
+    }
+
+    .layout > * {
+        margin: 0;
+    }
+
+    .title {
+
+    }
+
+    .number-of-songs {
+
+    }
 </style>
